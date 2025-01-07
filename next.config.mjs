9a687cd694f559415e4e56a680f1fb75611148a8
larrybwosi <larrybwosi@gmail.com>
2025-01-07 10:18:08 +0300

    next config fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { withPayload } from '@payloadcms/next/withPayload'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -37,4 +35,4 @@ const nextConfig = {
   }
 };
 
-export default withPayload(nextConfig) 
+export default nextConfig;
